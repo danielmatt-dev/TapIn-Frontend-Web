08d04eceee53fe1a358d4e460650ed69ec472998
iTHeBroncoi <angelvolkenturbo15@gmai.com>
2025-04-21 12:03:53 -0600

    Aplicacion de la estructura del proyecto, conforme al diagrama de arquitectura

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -1,16 +1,16 @@
 import { Routes } from '@angular/router';
-import { AppLayout } from './app/layout/component/app.layout';
-import { Notfound } from './app/pages/notfound/notfound';
+import { AppLayout } from './app/features/presentation/layout/component/app.layout';
+import { Notfound } from './app/features/presentation/pages/notfound/notfound';
 
 export const appRoutes: Routes = [
     { path: '', redirectTo: 'auth', pathMatch: 'full' },
-    { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') },
+    { path: 'auth', loadChildren: () => import('./app/features/presentation/pages/auth/auth.routes') },
 
     {
         path: '',
         component: AppLayout,
         children: [
-            { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
+            { path: 'pages', loadChildren: () => import('./app/features/presentation/pages/pages.routes') }
         ]
     },
     { path: 'notfound', component: Notfound },
