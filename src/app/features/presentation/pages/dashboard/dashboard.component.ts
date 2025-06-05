85b244b87772e4e9ec5babd88ba61db5379c7550
iTHeBroncoi <angelvolkenturbo15@gmai.com>
2025-06-05 08:26:10 -0600

    Sort table

--- src/app/features/presentation/pages/dashboard/dashboard.component.ts
+++ src/app/features/presentation/pages/dashboard/dashboard.component.ts
@@ -3,7 +3,7 @@ import { AppFloatingConfigurator } from '../../layout/component/app.floatingconf
 import { Select } from 'primeng/select';
 import { FormsModule } from '@angular/forms';
 import { Button } from 'primeng/button';
-import { PrimeTemplate } from 'primeng/api';
+import { PrimeTemplate, SortEvent } from 'primeng/api';
 import { NgIf } from '@angular/common';
 import { ChartModule, UIChart } from 'primeng/chart';
 import { ChartData, ChartOptions } from 'chart.js';
@@ -108,6 +108,25 @@ export class DashboardComponent implements OnInit {
 
     onRowSelect(){
 
+    }
+
+    customSort($event: SortEvent) {
+
+    }
+
+    sortTableData(event) {
+        event.data.sort((data1, data2) => {
+            let value1 = data1[event.field];
+            let value2 = data2[event.field];
+            let result = null;
+            if (value1 == null && value2 != null) result = -1;
+            else if (value1 != null && value2 == null) result = 1;
+            else if (value1 == null && value2 == null) result = 0;
+            else if (typeof value1 === 'string' && typeof value2 === 'string') result = value1.localeCompare(value2);
+            else result = value1 < value2 ? -1 : value1 > value2 ? 1 : 0;
+
+            return event.order * result;
+        });
     }
 }
 
